00e07e324cd7eaa12d633daf011cd5f36f32a970
ILOVEKKKK <18721291318@163.com>
2025-12-17 11:35:52 +0800

    finish_histogram

--- 直方图统计.cu
+++ 直方图统计.cu
@@ -21,3 +21,19 @@ __global__ void histogram_i32_kernel(int *a,int *y,int N)
         atomicAdd(&y[a[idx]],1);
     }
 }
+
+//INT32直方图统计+INT4向量化加载优化
+__global__ void histogram_i32x4_kernel(int *a, int *y, int N)
+{
+    int idx = 4*(blockIdx.x*blockDim.x+threadIdx.x);
+    if(idx < N)
+    {
+        //一次加载4个INT32类型数据
+        int4 reg_a = INT4(a[idx]);
+        atomicAdd(&(y[reg_a.x]),1);
+        atomicAdd(&(y[reg_a.y]),1);
+        atomicAdd(&(y[reg_a.z]),1);
+        atomicAdd(&(y[reg_a.w]),1);
+    }
+}
+
